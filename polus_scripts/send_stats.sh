d7bfce34bc02a20d94dcae57d6a53674224d33f4
gml-sync <robingood.m@gmail.com>
2021-01-19 19:09:54 +0300

    2

--- polus_scripts/send_stats.sh
+++ polus_scripts/send_stats.sh
@@ -1,5 +1,4 @@
-pwd
 cd runs
-#zip -r logbook.zip logbook
-#curl -T logbook.zip ftp://staro.drevo.si:8021
-#rm logbook.zip
+zip -r logbook.zip logbook
+curl -T logbook.zip ftp://staro.drevo.si:8021
+rm logbook.zip
