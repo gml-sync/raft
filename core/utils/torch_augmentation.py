0d7656bf8873e1664d93ea1d9e06eb3aa8d420a5
gml-sync <example@gmail.com>
2021-03-01 01:07:59 +0300

    5

--- core/utils/torch_augmentation.py
+++ core/utils/torch_augmentation.py
@@ -8,12 +8,12 @@ def coords_grid(batch, ht, wd): # copied from utils
 def check_out_of_bound(flow, occ):
     """ Add new occlusions for cropping/zoom """
     
-    batch_size, _c, height, width = flow.size()
-    u = flow[:, :1, :, :] # b (c=1) h w
-    v = flow[:, 1:, :, :]
-    grid = coords_grid(batch_size, height, width)
-    xx = grid[:, :1] # b (c=1) h w
-    yy = grid[:, 1:]
+    _c, height, width = flow.size()
+    u = flow[:1, :, :] # b (c=1) h w
+    v = flow[1:, :, :]
+    grid = coords_grid(1, height, width)
+    xx = grid[0, :1] # b (c=1) h w
+    yy = grid[0, 1:]
     xx = xx + u
     yy = yy + v
 
