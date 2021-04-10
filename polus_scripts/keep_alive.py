58e5551018b57a8a48b8b35f5f3f81162d0ffc0e
gml-sync <example@gmail.com>
2021-04-11 00:28:33 +0300

    3

--- polus_scripts/keep_alive.py
+++ polus_scripts/keep_alive.py
@@ -27,22 +27,26 @@ def datestr():
 
 # even if script is called from different folder, switch to containing folder
 parser = argparse.ArgumentParser()
-parser.add_argument('--expid', help="experiment name")
+parser.add_argument('--expid', required=True, help="experiment name")
 parser.add_argument('--single', action='store_true', help="launch job only once")
 args = parser.parse_args()
 
+
+if args.expid:
 print(f"experiment name={args.expid}\nsingle run={args.single}")
+outputs = "$TRAINHOME/git/runs/" + args.expid
+subp_bash("mkdir -p " + outputs)
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 #output = os.environ['OUTPUTS'] # this will raise an error because it's a dictionary
-file_path = f'{args.expid}/job_stderr.txt'
+file_path = f'{outputs}/job_stderr.txt'
 if os.path.exists(file_path):
     os.remove(file_path)
 
 while True:
     print(datestr(), args.expid, 'submit')
     subp_bash(
-        f'bsub -o {args.expid}/job_stdout.txt '
-        f'-e {args.expid}/job_stderr.txt -W 02:30 -q normal -gpu "num=1:mode=exclusive_process" '
+        f'bsub -o {outputs}/job_stdout.txt '
+        f'-e {outputs}/job_stderr.txt -W 02:30 -q normal -gpu "num=1:mode=exclusive_process" '
         f'bash $TRAINHOME/git/polus_scripts/start_net.sh {args.expid}')
     if args.single:
         print('single run')
